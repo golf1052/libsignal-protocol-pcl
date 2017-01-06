3aa3c6dc87550ff47fe30953ecc282a3336bb6ab
Sanders Lauture <golf1052@msn.com>
2017-01-05 20:58:05 -0500

    Add explicit vector for numericfingerprintgeneratortest
    Reflects
    https://github.com/WhisperSystems/libsignal-protocol-java/commit/87fae0f98332e98a32bbb82515428b4edeb4181f

--- signal-protocol-tests/fingerprint/NumericFingerprintGeneratorTest.cs
+++ signal-protocol-tests/fingerprint/NumericFingerprintGeneratorTest.cs
@@ -1,5 +1,5 @@
 /** 
- * Copyright (C) 2016 langboost
+ * Copyright (C) 2017 langboost, golf1052
  * 
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -25,6 +25,29 @@ namespace org.whispersystems.libsignal.fingerprint
     [TestClass]
     public class NumericFingerprintGeneratorTest
     {
+        private static readonly byte[] ALICE_IDENTITY = new byte[] { 0x05, 0x06, 0x86, 0x3b, 0xc6, 0x6d, 0x02, 0xb4, 0x0d, 0x27, 0xb8, 0xd4, 0x9c, 0xa7, 0xc0, 0x9e, 0x92, 0x39, 0x23, 0x6f, 0x9d, 0x7d, 0x25, 0xd6, 0xfc, 0xca, 0x5c, 0xe1, 0x3c, 0x70, 0x64, 0xd8, 0x68 };
+        private static readonly byte[] BOB_IDENTITY = new byte[] { 0x05, 0xf7, 0x81, 0xb6, 0xfb, 0x32, 0xfe, 0xd9, 0xba, 0x1c, 0xf2, 0xde, 0x97, 0x8d, 0x4d, 0x5d, 0xa2, 0x8d, 0xc3, 0x40, 0x46, 0xae, 0x81, 0x44, 0x02, 0xb5, 0xc0, 0xdb, 0xd9, 0x6f, 0xda, 0x90, 0x7b };
+        private static readonly string FINGERPRINT = "300354477692869396892869876765458257569162576843440918079131";
+
+        [TestMethod]
+        public void testVectors()
+        {
+            IdentityKey aliceIdentityKey = new IdentityKey(ALICE_IDENTITY, 0);
+            IdentityKey bobIdentityKey = new IdentityKey(BOB_IDENTITY, 0);
+
+            NumericFingerprintGenerator generator = new NumericFingerprintGenerator(5200);
+            Fingerprint aliceFingerprint = generator.createFor(
+                "+14152222222", aliceIdentityKey,
+                "+14153333333", bobIdentityKey);
+
+            Fingerprint bobFingerprint = generator.createFor(
+                "+14153333333", bobIdentityKey,
+                "+14152222222", aliceIdentityKey);
+
+            Assert.AreEqual(aliceFingerprint.getDisplayableFingerprint().getDisplayText(), FINGERPRINT);
+            Assert.AreEqual(bobFingerprint.getDisplayableFingerprint().getDisplayText(), FINGERPRINT);
+        }
+
         [TestMethod]
         public void testMatchingFingerprints()
         {
